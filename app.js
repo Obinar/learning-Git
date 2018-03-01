56992400a6f5adf6b8f5879021eb67677f85736f
Obinna <justobinar@gmail.com>
2018-03-01 12:42:45 +0100

    adds lesson on learning Git

---
+++ app.js
@@ -0,0 +1,5 @@
+document.querySelector('.btn').addEventListener('click', function () {
+  document.querySelector('.para').style.color      = 'red';
+  document.querySelector('h2').style.textTransform = 'uppercase';
+  document.querySelector('h2').style.textAlign     = 'center';
+})
