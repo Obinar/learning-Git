ad0cceb7e67df7dd69f50d69984f9512f6ce2538
Obinna <justobinar@gmail.com>
2018-03-01 12:54:54 +0100

    made changes to app.js file

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.querySelector('.btn').addEventListener('click', function () {
-  document.querySelector('.para').style.color      = 'red';
+  document.querySelector('.para').style.color      = 'blue';
   document.querySelector('h2').style.textTransform = 'uppercase';
-  document.querySelector('h2').style.textAlign     = 'center';
+  document.querySelector('h2').style.fontFamily    = 'verdana';
 })
